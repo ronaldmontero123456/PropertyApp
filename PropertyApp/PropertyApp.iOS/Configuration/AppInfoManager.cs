5e4a3c54308d6d062c4f8b7410fbcc6287a7c559
Rmontero <rmontero@mdsoft.edu.do>
2021-07-08 11:43:41 -0400

    ...

---
+++ PropertyApp/PropertyApp.iOS/Configuration/AppInfoManager.cs
@@ -0,0 +1,22 @@
+﻿using Foundation;
+using PropertyApp.Abstraction;
+using PropertyApp.iOS.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AppInfoManager))]
+namespace PropertyApp.iOS.Configuration
+{
+    public class AppInfoManager : IAppInfo
+    {
+        public string DatabasePath()
+        {
+            return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "..", "Library");
+        }
+    }
+}
