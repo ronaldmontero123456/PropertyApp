976e3d2f9694f5fb8f133797329bad0eceef2014
rmontero <mmateo@mdsoft.com.do>
2021-08-26 19:51:08 -0400

    ...

--- PropertyApp/PropertyApp/View/EmpleadosPage.xaml.cs
+++ PropertyApp/PropertyApp/View/EmpleadosPage.xaml.cs
@@ -1,12 +1,8 @@
 ﻿using PropertyApp.DataAccess;
 using PropertyApp.Models;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
